b439ae3fd7e67afe8947383207b435b97a78d2b8
Hajime Nakagami <nakagami@gmail.com>
2013-09-23 21:17:59 +0900

    rows and result

---
+++ result.go
@@ -0,0 +1,22 @@
+// Stub (dummy) Driver for Go's database/sql package
+// Written by Hajime Nakagami<nakagami@gmail.com>
+// Public Domain http://creativecommons.org/publicdomain/zero/1.0/
+
+package stubdriver
+
+type stubResult struct {
+    affectedRows int64
+    insertId     int64
+}
+
+func (res *stubResult) LastInsertId() (int64, error) {
+    return res.insertId, nil
+}
+
+func (res *stubResult) RowsAffected() (int64, error) {
+    return res.affectedRows, nil
+}
+
+func NewStubResult() *stubResult {
+    return new(stubResult)
+}
---
+++ rows.go
@@ -0,0 +1,42 @@
+// Stub (dummy) Driver for Go's database/sql package
+// Written by Hajime Nakagami<nakagami@gmail.com>
+// Public Domain http://creativecommons.org/publicdomain/zero/1.0/
+
+package stubdriver
+
+import (
+    "database/sql/driver"
+    "fmt"
+    "io"
+)
+
+type stubRows struct {
+    curNum int
+}
+
+func NewStubRows() *stubRows {
+    return new(stubRows)
+}
+
+func (rows *stubRows) Columns() ([]string) {
+    fmt.Println("stubRows.Columns()")
+    return []string{"Column1", "Column2"}
+}
+
+func (rows *stubRows) Close() (er error) {
+    fmt.Println("stubRows.Close()")
+    return
+}
+
+func (rows *stubRows) Next(dest []driver.Value) (err error) {
+    fmt.Println("stubRows.Next()")
+    rows.curNum++
+    if rows.curNum < 5 {
+        dest[0] = rows.curNum
+        dest[1] = "ABC"
+    } else {
+        err = io.EOF
+    }
+
+    return
+}
